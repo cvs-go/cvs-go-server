c0fb4b1464d440667aada2f581c21c8e84466fa2
chaewss <chaewseo12@gmail.com>
2022-12-08 01:22:50 +0900

    fix: 연관관계 추가 #3

--- src/main/java/com/cvsgo/entity/Review.java
+++ src/main/java/com/cvsgo/entity/Review.java
@@ -19,7 +19,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Getter
 @Builder
--- src/main/java/com/cvsgo/entity/ReviewImage.java
+++ src/main/java/com/cvsgo/entity/ReviewImage.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,5 +24,10 @@ public class ReviewImage {
     private Long id;
 
     private String imageUrl;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
 
 }
