779077d556e550e1b5273a0cdb8f254524a07764
chaewss <chaewseo12@gmail.com>
2022-12-08 01:20:30 +0900

    fix: Id Class 레포지토리 수정 #3

--- src/main/java/com/cvsgo/repository/UserLikeRepository.java
+++ src/main/java/com/cvsgo/repository/UserLikeRepository.java
@@ -1,8 +1,9 @@
 package com.cvsgo.repository;
 
 import com.cvsgo.entity.UserLike;
+import com.cvsgo.entity.UserLikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
+public interface UserLikeRepository extends JpaRepository<UserLike, UserLikeId> {
 
 }
