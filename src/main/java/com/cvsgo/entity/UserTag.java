3894ffe9cbeec4380f51018c1e34e1c0314927c4
feel-coding <khe628@naver.com>
2023-01-22 19:27:43 +0900

    refactor: UserTag에 지연로딩 적용 #5

--- src/main/java/com/cvsgo/entity/UserTag.java
+++ src/main/java/com/cvsgo/entity/UserTag.java
@@ -1,6 +1,7 @@
 package com.cvsgo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,11 +23,11 @@ public class UserTag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id")
     private Tag tag;
 
