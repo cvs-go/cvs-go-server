4a77ae90be3f45439a9309e112fe7b690a75522a
feel-coding <khe628@naver.com>
2023-01-25 02:30:13 +0900

    fix: RestControllerAdvice 어노테이션 추가

--- src/main/java/com/cvsgo/exception/user/UserExceptionHandler.java
+++ src/main/java/com/cvsgo/exception/user/UserExceptionHandler.java
@@ -4,7 +4,9 @@ import com.cvsgo.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class UserExceptionHandler {
 
     @ResponseStatus(HttpStatus.CONFLICT)
