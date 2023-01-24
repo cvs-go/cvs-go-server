18fcd2af1faac13a82fc3ac3d084a7f54d9f6aec
feel-coding <khe628@naver.com>
2023-01-24 17:56:25 +0900

    fix: 예외 타입 잘못 선언된 부분 수정 #5

--- src/main/java/com/cvsgo/exception/ExceptionConstants.java
+++ src/main/java/com/cvsgo/exception/ExceptionConstants.java
@@ -1,10 +1,11 @@
 package com.cvsgo.exception;
 
 import com.cvsgo.exception.user.DuplicateEmailException;
+import com.cvsgo.exception.user.DuplicateNicknameException;
 
 public interface ExceptionConstants {
 
     DuplicateEmailException DUPLICATE_EMAIL = new DuplicateEmailException(ErrorCode.DUPLICATE_EMAIL);
-    DuplicateEmailException DUPLICATE_NICKNAME = new DuplicateEmailException(ErrorCode.DUPLICATE_NICKNAME);
+    DuplicateNicknameException DUPLICATE_NICKNAME = new DuplicateNicknameException(ErrorCode.DUPLICATE_NICKNAME);
 
 }
