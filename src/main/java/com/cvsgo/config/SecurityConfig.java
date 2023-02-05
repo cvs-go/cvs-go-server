a58c15e7ffa7c23e908384da04d54ccc7d561084
feel-coding <khe628@naver.com>
2023-02-06 00:16:28 +0900

    refactor: 불필요한 어노테이션 `@EnableWebSecurity` 제거 #5

--- src/main/java/com/cvsgo/config/SecurityConfig.java
+++ src/main/java/com/cvsgo/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package com.cvsgo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
