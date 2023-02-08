04fac292a0376084f4c258433eb67cddee248899
feel-coding <khe628@naver.com>
2023-02-09 01:06:00 +0900

    refactor: passwordEncoder 빈 메소드명 수정 #5

--- src/main/java/com/cvsgo/config/SecurityConfig.java
+++ src/main/java/com/cvsgo/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    PasswordEncoder getPasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
