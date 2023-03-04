cfc2b46f9da5e6a1dce945d0d84b2c18e683fdd8
feel-coding <khe628@naver.com>
2023-03-04 16:28:48 +0900

    fix: 로그아웃 DTO에 매개변수 없는 생성자 추가 #8

--- src/main/java/com/cvsgo/dto/auth/LogoutRequestDto.java
+++ src/main/java/com/cvsgo/dto/auth/LogoutRequestDto.java
@@ -2,8 +2,10 @@ package com.cvsgo.dto.auth;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class LogoutRequestDto {
 
     private String token;
