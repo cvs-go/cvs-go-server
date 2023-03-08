e0e214d218ab281303535375f96890ff8be4768f
feel-coding <khe628@naver.com>
2023-03-08 14:48:29 +0900

    refactor: 숫자 가독성 있게 세자리마다 underscore 추가 #16

--- src/main/java/com/cvsgo/util/AuthConstants.java
+++ src/main/java/com/cvsgo/util/AuthConstants.java
@@ -4,8 +4,8 @@ public interface AuthConstants {
 
     String TOKEN_TYPE = "Bearer";
 
-    long ACCESS_TOKEN_TTL_MILLISECOND = 1000 * 60 * 30;
+    long ACCESS_TOKEN_TTL_MILLISECOND = 1_000 * 60 * 30;
 
-    long REFRESH_TOKEN_TTL_MILLISECOND = 1000 * 60 * 60 * 24 * 14;
+    long REFRESH_TOKEN_TTL_MILLISECOND = 1_000 * 60 * 60 * 24 * 14;
 
 }
