5a7e90c720e59bd9068cc14003e9498878071c0a
feel-coding <khe628@naver.com>
2023-03-15 15:36:40 +0900

    chore: index.html, ico 파일 인터셉터 로직 타지 않도록 제외 #16

--- src/main/java/com/cvsgo/config/WebConfig.java
+++ src/main/java/com/cvsgo/config/WebConfig.java
@@ -28,6 +28,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(authInterceptor)
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/api/auth/login", "/api/users", "/api/tags", "/api/users/emails/*/exists", "/api/users/nicknames/*/exists");
+                .excludePathPatterns("/", "/index.html", "/*.ico", "/api/auth/login", "/api/users", "/api/tags", "/api/users/emails/*/exists", "/api/users/nicknames/*/exists");
     }
 }
