f7b88d59796482a3a42806aaa4ec93b26637c152
chaewss <chaewseo12@gmail.com>
2023-03-17 17:22:56 +0900

    refactor: 모든 실행 환경 호환을 위해 @Param 사용 #17

--- src/main/java/com/cvsgo/repository/SellAtRepository.java
+++ src/main/java/com/cvsgo/repository/SellAtRepository.java
@@ -4,10 +4,11 @@ import com.cvsgo.entity.SellAt;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface SellAtRepository extends JpaRepository<SellAt, Long> {
 
     @EntityGraph(attributePaths = {"convenienceStore", "product"})
-    List<SellAt> findByProductId(Long productId);
+    List<SellAt> findByProductId(@Param("productId") Long productId);
 
 }
