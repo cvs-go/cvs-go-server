cac5f2e2eac0c69ae3350296b7ae5c379c9919da
chaewss <chaewseo12@gmail.com>
2023-03-31 03:07:09 +0900

    chore: timestamp Asia/Seoul 적용, 포맷 설정

--- src/main/java/com/cvsgo/dto/Response.java
+++ src/main/java/com/cvsgo/dto/Response.java
@@ -1,5 +1,6 @@
 package com.cvsgo.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 @Getter
 public class Response {
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private final LocalDateTime timestamp = LocalDateTime.now();
 
 }
