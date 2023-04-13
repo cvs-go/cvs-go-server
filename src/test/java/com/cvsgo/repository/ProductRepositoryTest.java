279bfa86687d25d624680a9dffff99ccf5faa004
chaewss <chaewseo12@gmail.com>
2023-04-13 17:47:30 +0900

    test: 데이터 INSERT 역할의 BeforeEach 메서드 생성 #45

--- src/test/java/com/cvsgo/repository/ProductRepositoryTest.java
+++ src/test/java/com/cvsgo/repository/ProductRepositoryTest.java
@@ -14,11 +14,12 @@ import com.cvsgo.entity.ConvenienceStore;
 import com.cvsgo.entity.DiscountEvent;
 import com.cvsgo.entity.Manufacturer;
 import com.cvsgo.entity.Product;
-import com.cvsgo.entity.Role;
 import com.cvsgo.entity.SellAt;
 import com.cvsgo.entity.User;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,24 +55,84 @@ class ProductRepositoryTest {
     @Autowired
     EventRepository eventRepository;
 
+    User user;
+    Product product1;
+    Product product2;
+    BogoEvent bogoEvent;
+    BtgoEvent btgoEvent;
+    DiscountEvent discountEvent;
+
+    @BeforeEach
+    void initData() {
+        user = User.create("abc@naver.com", "password1!", "닉네임", new ArrayList<>());
+        userRepository.save(user);
+
+        Category category = Category.builder()
+            .name("유제품")
+            .build();
+        categoryRepository.save(category);
+
+        Manufacturer manufacturer = Manufacturer.builder()
+            .name("칠성")
+            .build();
+        manufacturerRepository.save(manufacturer);
+
+        product1 = Product.builder()
+            .name("상품1")
+            .price(1000)
+            .category(category)
+            .manufacturer(manufacturer)
+            .build();
+
+        product2 = Product.builder()
+            .name("상품2")
+            .price(5000)
+            .category(category)
+            .manufacturer(manufacturer)
+            .build();
+        productRepository.saveAll(List.of(product1, product2));
+
+        ConvenienceStore cvs = ConvenienceStore.builder()
+            .name("GS25")
+            .build();
+        ConvenienceStore cvs2 = ConvenienceStore.builder()
+            .name("CU")
+            .build();
+        convenienceStoreRepository.saveAll(List.of(cvs, cvs2));
+
+        SellAt sellAt1 = SellAt.builder()
+            .product(product1)
+            .convenienceStore(cvs)
+            .build();
+        SellAt sellAt2 = SellAt.builder()
+            .product(product1)
+            .convenienceStore(cvs2)
+            .build();
+        SellAt sellAt3 = SellAt.builder()
+            .product(product2)
+            .convenienceStore(cvs)
+            .build();
+        sellAtRepository.saveAll(List.of(sellAt1, sellAt2, sellAt3));
+
+        bogoEvent = BogoEvent.builder()
+            .convenienceStore(cvs)
+            .product(product1)
+            .build();
+        discountEvent = DiscountEvent.builder()
+            .product(product1)
+            .convenienceStore(cvs2)
+            .discountAmount(500)
+            .build();
+        btgoEvent = BtgoEvent.builder()
+            .convenienceStore(cvs)
+            .product(product2)
+            .build();
+        eventRepository.saveAll(List.of(bogoEvent, discountEvent, btgoEvent));
+    }
+
     @Test
     @DisplayName("ID를 통해 상품을 조회한다")
     void succeed_to_find_product_by_id() {
-        // given
-        Product product1 = Product.builder()
-            .id(1L)
-            .name("상품1")
-            .price(1000)
-            .build();
-
-        Product product2 = Product.builder()
-            .id(2L)
-            .name("상품2")
-            .price(5000)
-            .build();
-
-        productRepository.saveAll(List.of(product1, product2));
-
         Optional<Product> foundProduct1 = productRepository.findById(product1.getId());
         Optional<Product> foundProduct2 = productRepository.findById(product2.getId());
 
@@ -82,23 +143,8 @@ class ProductRepositoryTest {
     @Test
     @DisplayName("상품 판매 편의점을 조회한다")
     void succeed_to_find_first_by_order_by_price_desc() {
-        // given
-        Product product1 = Product.builder()
-            .name("상품1")
-            .price(1000)
-            .build();
-
-        Product product2 = Product.builder()
-            .name("상품2")
-            .price(5000)
-            .build();
-
-        productRepository.saveAll(List.of(product1, product2));
-
-        // when
         Product highestPriceProduct = productRepository.findFirstByOrderByPriceDesc();
 
-        // then
         assertThat(highestPriceProduct.getPrice()).isEqualTo(product2.getPrice());
     }
 
@@ -107,68 +153,6 @@ class ProductRepositoryTest {
     void succeed_to_search_by_filter() {
         // given
         Pageable pageable = PageRequest.of(0, 20);
-
-        User user = User.builder()
-            .userId("abc@gmail.com")
-            .password("12345678a!")
-            .nickname("닉네임1")
-            .role(Role.ASSOCIATE)
-            .build();
-
-        Category category = Category.builder()
-            .name("유제품")
-            .build();
-
-        Manufacturer manufacturer = Manufacturer.builder()
-            .name("칠성")
-            .build();
-
-        Product product1 = Product.builder()
-            .name("상품1")
-            .price(1000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        Product product2 = Product.builder()
-            .name("상품2")
-            .price(5000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        ConvenienceStore cvs = ConvenienceStore.builder()
-            .name("GS25")
-            .build();
-
-        SellAt sellAt1 = SellAt.builder()
-            .product(product1)
-            .convenienceStore(cvs)
-            .build();
-
-        SellAt sellAt2 = SellAt.builder()
-            .product(product2)
-            .convenienceStore(cvs)
-            .build();
-
-        BogoEvent bogoEvent = BogoEvent.builder()
-            .convenienceStore(cvs)
-            .product(product1)
-            .build();
-
-        BtgoEvent btgoEvent = BtgoEvent.builder()
-            .convenienceStore(cvs)
-            .product(product2)
-            .build();
-
-        userRepository.save(user);
-        categoryRepository.save(category);
-        manufacturerRepository.save(manufacturer);
-        productRepository.saveAll(List.of(product1, product2));
-        convenienceStoreRepository.save(cvs);
-        sellAtRepository.saveAll(List.of(sellAt1, sellAt2));
-        eventRepository.saveAll(List.of(bogoEvent, btgoEvent));
-
         SearchProductRequestDto request = SearchProductRequestDto.builder()
             .convenienceStoreIds(null)
             .categoryIds(null)
@@ -190,10 +174,10 @@ class ProductRepositoryTest {
         // when
         List<SearchProductQueryDto> foundProducts = productRepository.searchByFilter(user, request,
             pageable);
-        List<Long> foundProductIds = foundProducts.stream().map(SearchProductQueryDto::getProductId)
-            .toList();
 
         // then
+        List<Long> foundProductIds = foundProducts.stream().map(SearchProductQueryDto::getProductId)
+            .toList();
         assertThat(productResponse1.getProductId()).isIn(foundProductIds);
         assertThat(productResponse2.getProductId()).isNotIn(foundProductIds);
     }
@@ -202,59 +186,6 @@ class ProductRepositoryTest {
     @DisplayName("상품 판매 편의점 목록 요소의 전체 개수를 조회한다")
     void succeed_to_count_by_filter() {
         // given
-        Category category = Category.builder()
-            .name("유제품")
-            .build();
-
-        Manufacturer manufacturer = Manufacturer.builder()
-            .name("칠성")
-            .build();
-
-        Product product1 = Product.builder()
-            .name("상품1")
-            .price(1000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        Product product2 = Product.builder()
-            .name("상품2")
-            .price(5000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        ConvenienceStore cvs = ConvenienceStore.builder()
-            .name("GS25")
-            .build();
-
-        SellAt sellAt1 = SellAt.builder()
-            .product(product1)
-            .convenienceStore(cvs)
-            .build();
-
-        SellAt sellAt2 = SellAt.builder()
-            .product(product2)
-            .convenienceStore(cvs)
-            .build();
-
-        BogoEvent bogoEvent = BogoEvent.builder()
-            .convenienceStore(cvs)
-            .product(product1)
-            .build();
-
-        BtgoEvent btgoEvent = BtgoEvent.builder()
-            .convenienceStore(cvs)
-            .product(product2)
-            .build();
-
-        categoryRepository.save(category);
-        manufacturerRepository.save(manufacturer);
-        productRepository.saveAll(List.of(product1, product2));
-        convenienceStoreRepository.save(cvs);
-        sellAtRepository.saveAll(List.of(sellAt1, sellAt2));
-        eventRepository.saveAll(List.of(bogoEvent, btgoEvent));
-
         SearchProductRequestDto request = SearchProductRequestDto.builder()
             .convenienceStoreIds(null)
             .categoryIds(null)
@@ -275,74 +206,6 @@ class ProductRepositoryTest {
     @DisplayName("상품 ID 리스트에 따른 편의점 행사를 조회한다")
     void succeed_to_find_convenience_store_events_by_product_ids() {
         // given
-        Category category = Category.builder()
-            .name("유제품")
-            .build();
-
-        Manufacturer manufacturer = Manufacturer.builder()
-            .name("칠성")
-            .build();
-
-        Product product1 = Product.builder()
-            .name("상품1")
-            .price(1000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        Product product2 = Product.builder()
-            .name("상품2")
-            .price(5000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        ConvenienceStore cvs = ConvenienceStore.builder()
-            .name("GS25")
-            .build();
-
-        ConvenienceStore cvs2 = ConvenienceStore.builder()
-            .name("CU")
-            .build();
-
-        SellAt sellAt1 = SellAt.builder()
-            .product(product1)
-            .convenienceStore(cvs)
-            .build();
-
-        SellAt sellAt2 = SellAt.builder()
-            .product(product1)
-            .convenienceStore(cvs2)
-            .build();
-
-        SellAt sellAt3 = SellAt.builder()
-            .product(product2)
-            .convenienceStore(cvs)
-            .build();
-
-        BogoEvent bogoEvent = BogoEvent.builder()
-            .convenienceStore(cvs)
-            .product(product1)
-            .build();
-
-        DiscountEvent discountEvent = DiscountEvent.builder()
-            .product(product1)
-            .convenienceStore(cvs2)
-            .discountAmount(500)
-            .build();
-
-        BtgoEvent btgoEvent = BtgoEvent.builder()
-            .convenienceStore(cvs)
-            .product(product2)
-            .build();
-
-        categoryRepository.save(category);
-        manufacturerRepository.save(manufacturer);
-        productRepository.saveAll(List.of(product1, product2));
-        convenienceStoreRepository.saveAll(List.of(cvs, cvs2));
-        sellAtRepository.saveAll(List.of(sellAt1, sellAt2, sellAt3));
-        eventRepository.saveAll(List.of(bogoEvent, discountEvent, btgoEvent));
-
         ConvenienceStoreEventQueryDto product1CvsEvent1 =
             new ConvenienceStoreEventQueryDto(bogoEvent.getProduct().getId(),
                 bogoEvent.getConvenienceStore().getName(), bogoEvent);
@@ -372,100 +235,23 @@ class ProductRepositoryTest {
     @DisplayName("상품 ID를 통해 상품 정보를 조회한다")
     void succeed_to_find_by_product_id() {
         // given
-        User user = User.builder()
-            .userId("abc@gmail.com")
-            .password("12345678a!")
-            .nickname("닉네임1")
-            .role(Role.ASSOCIATE)
-            .build();
-
-        Category category = Category.builder()
-            .name("유제품")
-            .build();
-
-        Manufacturer manufacturer = Manufacturer.builder()
-            .name("칠성")
-            .build();
-
-        Product product = Product.builder()
-            .name("상품1")
-            .price(1000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        userRepository.save(user);
-        categoryRepository.save(category);
-        manufacturerRepository.save(manufacturer);
-        productRepository.save(product);
-
         SearchProductDetailQueryDto productDetailResponse = new SearchProductDetailQueryDto(
-            product.getId(), product.getName(), product.getPrice(), product.getImageUrl(),
-            product.getManufacturer().getName(), null, null);
+            product1.getId(), product1.getName(), product1.getPrice(), product1.getImageUrl(),
+            product1.getManufacturer().getName(), null, null);
 
         // when
         Optional<SearchProductDetailQueryDto> foundProduct = productRepository.findByProductId(user,
-            product.getId());
+            product1.getId());
 
         // then
-        assertThat(foundProduct.get().getProductId()).isEqualTo(product.getId());
+        assertThat(foundProduct.get().getProductId()).isEqualTo(
+            productDetailResponse.getProductId());
     }
 
     @Test
     @DisplayName("상품 ID에 따른 편의점 행사를 조회한다")
     void succeed_to_find_convenience_store_events_by_product_id() {
         // given
-        Category category = Category.builder()
-            .name("유제품")
-            .build();
-
-        Manufacturer manufacturer = Manufacturer.builder()
-            .name("칠성")
-            .build();
-
-        Product product = Product.builder()
-            .name("상품1")
-            .price(1000)
-            .category(category)
-            .manufacturer(manufacturer)
-            .build();
-
-        ConvenienceStore cvs = ConvenienceStore.builder()
-            .name("GS25")
-            .build();
-
-        ConvenienceStore cvs2 = ConvenienceStore.builder()
-            .name("CU")
-            .build();
-
-        SellAt sellAt1 = SellAt.builder()
-            .product(product)
-            .convenienceStore(cvs)
-            .build();
-
-        SellAt sellAt2 = SellAt.builder()
-            .product(product)
-            .convenienceStore(cvs2)
-            .build();
-
-        BogoEvent bogoEvent = BogoEvent.builder()
-            .convenienceStore(cvs)
-            .product(product)
-            .build();
-
-        DiscountEvent discountEvent = DiscountEvent.builder()
-            .product(product)
-            .convenienceStore(cvs2)
-            .discountAmount(500)
-            .build();
-
-        categoryRepository.save(category);
-        manufacturerRepository.save(manufacturer);
-        productRepository.save(product);
-        convenienceStoreRepository.saveAll(List.of(cvs, cvs2));
-        sellAtRepository.saveAll(List.of(sellAt1, sellAt2));
-        eventRepository.saveAll(List.of(bogoEvent, discountEvent));
-
         ConvenienceStoreEventQueryDto product1CvsEvent1 =
             new ConvenienceStoreEventQueryDto(bogoEvent.getProduct().getId(),
                 bogoEvent.getConvenienceStore().getName(), bogoEvent);
@@ -475,7 +261,7 @@ class ProductRepositoryTest {
 
         // when
         List<ConvenienceStoreEventQueryDto> foundProducts = productRepository.findConvenienceStoreEventsByProductId(
-            product.getId());
+            product1.getId());
 
         // then
         assertThat(foundProducts).hasSize(2);
