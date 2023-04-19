a8a1db256544f1f5313978b8a9bdfb759aab9881
chaewss <chaewseo12@gmail.com>
2023-04-20 02:49:40 +0900

    style: 코드 포맷팅 #48

--- src/test/java/com/cvsgo/controller/UserControllerTest.java
+++ src/test/java/com/cvsgo/controller/UserControllerTest.java
@@ -1,37 +1,5 @@
 package com.cvsgo.controller;
 
-import com.cvsgo.argumentresolver.LoginUserArgumentResolver;
-import com.cvsgo.config.WebConfig;
-import com.cvsgo.dto.user.SignUpRequestDto;
-import com.cvsgo.dto.user.SignUpResponseDto;
-import com.cvsgo.entity.Role;
-import com.cvsgo.entity.Tag;
-import com.cvsgo.entity.User;
-import com.cvsgo.exception.ExceptionConstants;
-import com.cvsgo.interceptor.AuthInterceptor;
-import com.cvsgo.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.cvsgo.ApiDocumentUtils.documentIdentifier;
 import static com.cvsgo.ApiDocumentUtils.getDocumentRequest;
 import static com.cvsgo.ApiDocumentUtils.getDocumentResponse;
@@ -51,6 +19,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.SharedHttpSessionConfigurer.sharedHttpSession;
+
+import com.cvsgo.argumentresolver.LoginUserArgumentResolver;
+import com.cvsgo.config.WebConfig;
+import com.cvsgo.dto.user.SignUpRequestDto;
+import com.cvsgo.dto.user.SignUpResponseDto;
+import com.cvsgo.entity.Tag;
+import com.cvsgo.entity.User;
+import com.cvsgo.exception.ExceptionConstants;
+import com.cvsgo.interceptor.AuthInterceptor;
+import com.cvsgo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(UserController.class)
@@ -75,10 +73,10 @@ class UserControllerTest {
     @BeforeEach
     void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .apply(sharedHttpSession())
-                .addFilters(new CharacterEncodingFilter("UTF-8", true))
-                .build();
+            .apply(documentationConfiguration(restDocumentation))
+            .apply(sharedHttpSession())
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .build();
     }
 
     @ParameterizedTest
@@ -86,16 +84,16 @@ class UserControllerTest {
     @ValueSource(strings = {"abc", "11111", "email1234", "@gmail.com", "abc1234@"})
     void respond_400_when_email_does_not_match_email_format(String email) throws Exception {
         SignUpRequestDto signUpRequest = SignUpRequestDto.builder()
-                .email(email)
-                .password("11111111aa!!")
-                .nickname("닉네임")
-                .tagIds(getTags().stream().map(Tag::getId).toList())
-                .build();
+            .email(email)
+            .password("11111111aa!!")
+            .nickname("닉네임")
+            .tagIds(getTags().stream().map(Tag::getId).toList())
+            .build();
 
         mockMvc.perform(post("/api/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isBadRequest()).andDo(print());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+            .andExpect(status().isBadRequest()).andDo(print());
     }
 
     @ParameterizedTest
@@ -104,16 +102,16 @@ class UserControllerTest {
     @NullAndEmptySource
     void respond_400_when_length_of_password_does_not_meet_password_policy(String password) throws Exception {
         SignUpRequestDto signUpRequest = SignUpRequestDto.builder()
-                .email("abc@naver.com")
-                .password(password)
-                .nickname("닉네임")
-                .tagIds(getTags().stream().map(Tag::getId).toList())
-                .build();
+            .email("abc@naver.com")
+            .password(password)
+            .nickname("닉네임")
+            .tagIds(getTags().stream().map(Tag::getId).toList())
+            .build();
 
         mockMvc.perform(post("/api/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isBadRequest()).andDo(print());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+            .andExpect(status().isBadRequest()).andDo(print());
     }
 
     @ParameterizedTest
@@ -121,16 +119,16 @@ class UserControllerTest {
     @ValueSource(strings = {"123456789a", "a123456789", "123456789!", "!123456789", "abcdefghijk", "12345678910", "1~!@#$%^&*()", "~!~!~!~!~!"})
     void respond_400_when_password_does_not_meet_password_policy(String password) throws Exception {
         SignUpRequestDto signUpRequest = SignUpRequestDto.builder()
-                .email("abc@naver.com")
-                .password(password)
-                .nickname("닉네임")
-                .tagIds(getTags().stream().map(Tag::getId).toList())
-                .build();
+            .email("abc@naver.com")
+            .password(password)
+            .nickname("닉네임")
+            .tagIds(getTags().stream().map(Tag::getId).toList())
+            .build();
 
         mockMvc.perform(post("/api/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isBadRequest()).andDo(print());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+            .andExpect(status().isBadRequest()).andDo(print());
     }
 
     @ParameterizedTest
@@ -138,17 +136,17 @@ class UserControllerTest {
     @ValueSource(strings = {"김", "닉", "열글자닉네임을만들자", "123456789"})
     void respond_400_when_length_of_nickname_does_not_meet_nickname_policy(String nickname) throws Exception {
         SignUpRequestDto signUpRequest = SignUpRequestDto.builder()
-                .email("abc@naver.com")
-                .password("111111111a!")
-                .nickname(nickname)
-                .tagIds(getTags().stream().map(Tag::getId).toList())
-                .build();
+            .email("abc@naver.com")
+            .password("111111111a!")
+            .nickname(nickname)
+            .tagIds(getTags().stream().map(Tag::getId).toList())
+            .build();
 
         mockMvc.perform(post("/api/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+            .andExpect(status().isBadRequest())
+            .andDo(print());
     }
 
     @Test
@@ -159,65 +157,65 @@ class UserControllerTest {
         given(userService.signUp(any())).willReturn(createResponse());
 
         mockMvc.perform(post("/api/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isCreated())
-                .andDo(print())
-                .andDo(document(documentIdentifier,
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        requestFields(
-                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
-                                fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
-                                fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
-                                fieldWithPath("tagIds").type(JsonFieldType.ARRAY).description("태그 ID 목록")
-                        ),
-                        responseFields(
-                                fieldWithPath("timestamp").type(JsonFieldType.STRING).description("요청 시각"),
-                                fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
-                                fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("닉네임"),
-                                fieldWithPath("data.role").type(JsonFieldType.STRING).description("회원 등급"),
-                                fieldWithPath("data.tagIds").type(JsonFieldType.ARRAY).description("태그 ID 목록")
-                        )
-                ));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+            .andExpect(status().isCreated())
+            .andDo(print())
+            .andDo(document(documentIdentifier,
+                getDocumentRequest(),
+                getDocumentResponse(),
+                requestFields(
+                    fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                    fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
+                    fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
+                    fieldWithPath("tagIds").type(JsonFieldType.ARRAY).description("태그 ID 목록")
+                ),
+                responseFields(
+                    fieldWithPath("timestamp").type(JsonFieldType.STRING).description("요청 시각"),
+                    fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                    fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("닉네임"),
+                    fieldWithPath("data.role").type(JsonFieldType.STRING).description("회원 등급"),
+                    fieldWithPath("data.tagIds").type(JsonFieldType.ARRAY).description("태그 ID 목록")
+                )
+            ));
     }
 
     @Test
     @DisplayName("해당 닉네임을 가진 계정이 존재하면 회원가입 API 호출시 HTTP 409를 응답한다")
     void respond_409_when_nickname_conflicts() throws Exception {
         SignUpRequestDto signUpRequest = SignUpRequestDto.builder()
-                .email("abc@naver.com")
-                .password("111111111a!")
-                .nickname("닉네임")
-                .tagIds(getTags().stream().map(Tag::getId).toList())
-                .build();
+            .email("abc@naver.com")
+            .password("111111111a!")
+            .nickname("닉네임")
+            .tagIds(getTags().stream().map(Tag::getId).toList())
+            .build();
 
         given(userService.signUp(any())).willThrow(ExceptionConstants.DUPLICATE_NICKNAME);
 
         mockMvc.perform(post("/api/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isConflict())
-                .andDo(print());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+            .andExpect(status().isConflict())
+            .andDo(print());
     }
 
     @Test
     @DisplayName("해당 이메일을 가진 계정이 존재하면 회원가입 API 호출시 HTTP 409를 응답한다")
     void respond_409_when_email_conflicts() throws Exception {
         SignUpRequestDto signUpRequest = SignUpRequestDto.builder()
-                .email("abc@naver.com")
-                .password("111111111a!")
-                .nickname("닉네임")
-                .tagIds(getTags().stream().map(Tag::getId).toList())
-                .build();
+            .email("abc@naver.com")
+            .password("111111111a!")
+            .nickname("닉네임")
+            .tagIds(getTags().stream().map(Tag::getId).toList())
+            .build();
 
         given(userService.signUp(any())).willThrow(ExceptionConstants.DUPLICATE_EMAIL);
 
         mockMvc.perform(post("/api/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isConflict())
-                .andDo(print());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+            .andExpect(status().isConflict())
+            .andDo(print());
     }
 
     @Test
@@ -227,20 +225,20 @@ class UserControllerTest {
         given(userService.isDuplicatedEmail(email)).willReturn(true);
 
         mockMvc.perform(get("/api/users/emails/{email}/exists", email).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("true")))
-                .andDo(print())
-                .andDo(document(documentIdentifier,
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        pathParameters(
-                                parameterWithName("email").description("이메일")
-                        ),
-                        responseFields(
-                                fieldWithPath("timestamp").type(JsonFieldType.STRING).description("요청 시각"),
-                                fieldWithPath("data").type(JsonFieldType.BOOLEAN).description("해당 이메일로 가입된 계정 존재 여부")
-                        )
-                ));
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("true")))
+            .andDo(print())
+            .andDo(document(documentIdentifier,
+                getDocumentRequest(),
+                getDocumentResponse(),
+                pathParameters(
+                    parameterWithName("email").description("이메일")
+                ),
+                responseFields(
+                    fieldWithPath("timestamp").type(JsonFieldType.STRING).description("요청 시각"),
+                    fieldWithPath("data").type(JsonFieldType.BOOLEAN).description("해당 이메일로 가입된 계정 존재 여부")
+                )
+            ));
     }
 
     @Test
@@ -250,9 +248,9 @@ class UserControllerTest {
         given(userService.isDuplicatedNickname(nickname)).willReturn(false);
 
         mockMvc.perform(get("/api/users/nicknames/{nickname}/exists", nickname).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("false")))
-                .andDo(print());
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("false")))
+            .andDo(print());
     }
 
     @Test
@@ -262,20 +260,20 @@ class UserControllerTest {
         given(userService.isDuplicatedNickname(nickname)).willReturn(true);
 
         mockMvc.perform(get("/api/users/nicknames/{nickname}/exists", nickname).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("true")))
-                .andDo(print())
-                .andDo(document(documentIdentifier,
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        pathParameters(
-                                parameterWithName("nickname").description("닉네임")
-                        ),
-                        responseFields(
-                                fieldWithPath("timestamp").type(JsonFieldType.STRING).description("요청 시각"),
-                                fieldWithPath("data").type(JsonFieldType.BOOLEAN).description("해당 닉네임으로 가입된 계정 존재 여부")
-                        )
-                ));
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("true")))
+            .andDo(print())
+            .andDo(document(documentIdentifier,
+                getDocumentRequest(),
+                getDocumentResponse(),
+                pathParameters(
+                    parameterWithName("nickname").description("닉네임")
+                ),
+                responseFields(
+                    fieldWithPath("timestamp").type(JsonFieldType.STRING).description("요청 시각"),
+                    fieldWithPath("data").type(JsonFieldType.BOOLEAN).description("해당 닉네임으로 가입된 계정 존재 여부")
+                )
+            ));
     }
 
     @Test
@@ -285,9 +283,9 @@ class UserControllerTest {
         given(userService.isDuplicatedEmail(email)).willReturn(false);
 
         mockMvc.perform(get("/api/users/emails/{email}/exists", email).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("false")))
-                .andDo(print());
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("false")))
+            .andDo(print());
     }
 
     private List<Tag> getTags() {
@@ -300,11 +298,11 @@ class UserControllerTest {
 
     private SignUpRequestDto createRequest() {
         return SignUpRequestDto.builder()
-                .email("abc@naver.com")
-                .password("111111111a!")
-                .nickname("닉네임")
-                .tagIds(getTags().stream().map(Tag::getId).toList())
-                .build();
+            .email("abc@naver.com")
+            .password("111111111a!")
+            .nickname("닉네임")
+            .tagIds(getTags().stream().map(Tag::getId).toList())
+            .build();
     }
 
     private User createUser() {
