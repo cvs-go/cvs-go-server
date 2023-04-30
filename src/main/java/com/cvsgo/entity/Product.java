31e485eddb7d1deac0f4687bb4a736a973c307f3
chaewss <chaewseo12@gmail.com>
2023-04-30 14:02:05 +0900

    refactor: 필요하지 않은 어노테이션 삭제 #63

--- src/main/java/com/cvsgo/entity/Product.java
+++ src/main/java/com/cvsgo/entity/Product.java
@@ -14,11 +14,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.DynamicInsert;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@DynamicInsert
 @Entity
 public class Product extends BaseTimeEntity {
 
