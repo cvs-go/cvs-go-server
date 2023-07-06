f8a79513bd25831cb20b99dbf859ed9f320d201b
chaewss <chaewseo12@gmail.com>
2023-07-06 17:15:07 +0900

    refactor: 불필요한 어노테이션 삭제 #79

--- src/main/java/com/cvsgo/dto/product/ReadLikedProductRequestDto.java
+++ src/main/java/com/cvsgo/dto/product/ReadLikedProductRequestDto.java
@@ -1,13 +1,10 @@
 package com.cvsgo.dto.product;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.Getter;
 
 @Getter
 public class ReadLikedProductRequestDto {
 
-    @Enumerated(EnumType.STRING)
     private final ProductSortBy sortBy;
 
     public ReadLikedProductRequestDto(ProductSortBy sortBy) {
